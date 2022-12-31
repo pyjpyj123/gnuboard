9b5ff96c9fe19bfcc1b0949256a8a4be4368cd42
pyjpyj123 <jindna@naver.com>
2022-12-31 23:38:09 +0900

    바로가기2

--- theme/community/head.php
+++ theme/community/head.php
@@ -150,9 +150,9 @@ include_once(G5_THEME_LIB_PATH.'/new_lastest.lib.php');
 	    <div id="aside_ct">
 	        <div class="short_cut">
 	            <ul>
-	                <li class="f_short_cut"><strong><a href="http://naver.com">내 정보 수정 1</a></strong></li>
+	                <li class="f_short_cut"><strong><a href="http://naver.com">내 정보 수정</a></strong></li>
 	                <li><strong><a href="http://3.39.240.45/bbs/board.php?bo_table=gallery">갤러리</a></strong></li>
-	                <li><strong><a href="#">헬로 헬로</a></strong></li>
+	                <li><strong><a href="#">안녕하세요</a></strong></li>
 	            </ul>
 	        </div>
 	        <div class="aside_lt">
